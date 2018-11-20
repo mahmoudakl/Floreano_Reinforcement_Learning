8e23762f7c2a21300fec1a07130d735a241d26d5
Mahmoud Akl <mahmoud.akl@gmail.com>
2018-11-20 13:34:35 +0100

    Completed infrastructure for evolutionary experiment

---
+++ evolution_utils.py
@@ -0,0 +1,236 @@
+import os
+import csv
+import numpy as np
+# This file contains helper functions to support running the Floreano Experiment
+
+
+def get_wheel_speeds(individual_dir, corrected=False):
+    """
+    """
+    if not corrected:
+        file_path = individual_dir + '/wheel_speeds.csv'
+    else:
+        file_path = individual_dir + '/corrected_wheel_speeds.csv'
+    return [i.strip().split(',') for i in open(file_path).readlines()][1:401]
+
+
+def get_trajectory(individual_dir):
+    file_path = individual_dir + '/robot_position.csv'
+    return [i.strip().split(',') for i in open(file_path).readlines()][1:401]
+
+
+def fitness_function(wheel_speeds):
+    """
+    Calculates the fitness function described in the Floreano paper
+    according to the saved wheel speeds
+    """
+    left_wheel = [float(t[1]) for t in wheel_speeds]
+    right_wheel = [float(t[2]) for t in wheel_speeds]
+    fitness = 0
+    for i in range(len(left_wheel)):
+        if left_wheel[i] > 0 and right_wheel[i] > 0:
+                   fitness += (left_wheel[i] + right_wheel[i])
+    print len(left_wheel)
+    return fitness/float(2*len(left_wheel))
+
+
+def get_top_performers(generation_dir, num_performers=15):
+    """
+    Extract the indices of the top individuals from the fitness log
+    
+    :param generation_dir: Directory where all generation results are stored
+    :param num_performers: number for top performers to look for. Default value
+                           is 15, which corresponds to a truncation threshold of
+                           25% in the original experiment
+    """
+    fitness_log = []
+    top_performers_indices = []
+    top_performers_individuals = []
+
+    # store all fitness values in a list
+    for i in range(60):
+        fitness_log.append(np.load(generation_dir + '/individual_{}'.format(i)
+            + '/fitness_value.npy'))
+
+    # save generation average fitness value
+    avg_fitness = np.average(fitness_log)
+    np.save(generation_dir + '/average_fitness', avg_fitness)
+
+    # find the top performing individuals
+    for i in range(num_performers):
+        max_index = np.argmax(fitness_log)
+        top_performers_indices.append(max_index)
+        top_performers_individuals.append(np.load(generation_dir +
+            '/individual_{}'.format(max_index) + '/genetic_string.npy'))
+        fitness_log[max_index] = -1
+
+    np.save(generation_dir + '/top_performers_indices', top_performers_indices)
+    np.save(generation_dir + '/top_performers_individuals',
+        top_performers_individuals)
+
+
+def one_point_crossover(parent1, parent2):
+    """
+    Performs one-point cross over on two genetic strings at a random point and
+    returns two new strings
+
+    :param parent1: The first string
+    :param parent2: The second string
+    """
+    parent1 = parent1.reshape(290)
+    parent2 = parent2.reshape(290)
+    child1 = np.zeros(290, dtype=int)
+    child2 = np.zeros(290, dtype=int)
+    point = np.random.randint(len(parent1))
+    for i in range(point):
+        child1[i] = parent1[i]
+        child2[i] = parent2[i]
+    for i in range(point, 290):
+        child1[i] = parent2[i]
+        child2[i] = parent1[i]
+    child1 = child1.reshape(10, 29)
+    child2 = child2.reshape(10, 29)
+    return child1, child2
+
+
+def bit_mutation(population):
+    """
+    Performs bit mutation on every item of every binary genetic string in a
+    population with 5% probability
+
+    :param population: A list of binary genetic strings 
+    """
+    for individual in population:
+        individual = individual.reshape(290)
+        for i in range(290):
+            if np.random.rand() < 0.05:
+                individual[i] = 0 if individual[i] else 1
+    return population
+
+
+def get_unique_pairs(population):
+    """
+    Returns all unique pairs in a list of items as a list of tuples
+
+    :param population: List of items
+    """
+    pairs = []
+    for i in range(len(population)):
+        for j in range(i+1, len(population)):
+            pairs.append((i, j))
+    return pairs
+
+
+def evolve_new_generation(generation_dir):
+    """
+    Evolve a new generation based on the top performers of a previous generation
+    using bt mutation, one-point cross over and elitism
+
+    :param top_performers: A list containing the best performing 25% individuals
+                           of a population
+    """
+    top_performers = np.load(generation_dir + '/top_performers_individuals.npy')
+    population = []
+    for i in range(len(top_performers)):
+        for j in range(4):
+            population.append(top_performers[i])
+    pairs = get_unique_pairs(population)
+    for i in pairs:
+        if np.random.rand() < 0.1:
+            parent1 = population[i[0]]
+            parent2 = population[i[1] - 1]
+            child1, child2 = one_point_crossover(parent1, parent2)
+            population[i[0]] = child1
+            population[i[1]] = child2
+
+    population = bit_mutation(population)
+    rand = np.random.randint(len(population))
+    population[rand] = top_performers[0]
+
+    file = open("populations.txt","w") 
+    file.write(str(population))
+    file.write("\n")
+    file.close() 
+
+    return population
+
+
+def correct_for_collisions(individual_dir):
+    wheel_speeds = get_wheel_speeds(individual_dir)
+    trajectory = get_trajectory(individual_dir)
+    x_axis = [x[0] for x in trajectory]
+    y_axis = [y[1] for y in trajectory]
+    collision = False
+
+    for i in range(len(wheel_speeds)):
+        if float(y_axis[i]) >= 2.4 or float(y_axis[i]) <= -2.4:
+            wheel_speeds[i][1] = '0.0'
+            collision = True
+        elif float(x_axis[i]) >= 3 or float(x_axis[i]) <= -3:
+            wheel_speeds[i][0] = '0.0'
+            collision = True
+
+    # save corrected wheel speeds
+    np.save(individual_dir + '/corrected_wheel_speeds', wheel_speeds)
+    return collision
+
+
+brain = """from hbp_nrp_cle.brainsim import simulator as sim
+import numpy as np
+import logging
+
+logger = logging.getLogger(__name__)
+
+dna = np.array([int(x) for x in '%s'.split(',')]).reshape(10, 29)
+
+receptors = []
+for r in range(1,19):
+    receptors.append(np.nonzero(dna[:,r])[0])
+
+
+def create_brain():
+
+    NEURONPARAMS = {'v_rest': -60.5,
+                    'tau_m': 4.0,
+                    'tau_refrac': 2.0,
+                    'tau_syn_E': 10.0,
+                    'tau_syn_I': 10.0,
+                    'e_rev_E': 0.0,
+                    'e_rev_I': -75.0,
+                    'v_thresh': -60.4,
+                    'v_reset': -60.5}
+
+    SYNAPSE_PARAMS = {"weight": 1.0,
+                      "delay": 2.0}
+
+    population = sim.Population(10, sim.IF_cond_alpha())
+    population[0:10].set(**NEURONPARAMS)
+
+
+    # Connect neurons
+    CIRCUIT = population
+
+    SYN = sim.StaticSynapse(**SYNAPSE_PARAMS)
+
+    row_counter=0
+    for row in dna:
+        logger.info(row)
+        n = np.array(row)
+        r_type = 'excitatory'
+        if n[0]==0:
+            r_type = 'inhibitory'
+        for i in range(19,29):
+            if n[i]==1:
+                sim.Projection(presynaptic_population=CIRCUIT[row_counter:1+row_counter], postsynaptic_population=CIRCUIT[i-19:i-18], connector=sim.OneToOneConnector(), synapse_type=SYN, receptor_type=r_type)
+        
+        row_counter+=1
+
+    sim.initialize(population, v=population.get('v_rest'))
+
+    logger.debug("Circuit description: " + str(population.describe()))
+
+    return population
+
+
+circuit = create_brain()
+"""
---
+++ plot_utils.py
@@ -0,0 +1,75 @@
+import csv
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def plot_trajectory(individual_dir, trajectory):
+	"""
+	
+	"""
+	fig = plt.figure()
+	plt.gca()
+	plt.xticks([], [])
+	plt.yticks([], [])
+	plt.ylim(-3, 3)
+	plt.xlim(-3.9, 3.9)
+	x_axis = [x[0] for x in trajectory]
+	y_axis = [y[1] for y in trajectory]
+	plt.plot([float(x) for x in x_axis], [float(y) for y in y_axis])
+	fig.savefig(individual_dir + '/trajectory.png')
+	plt.close()
+
+
+def plot_wheel_speeds(individual_dir, wheel_speeds):
+	"""
+	Reads the saved wheel speeds csv file and saves a plotted figure. It also
+	returns the wheel speeds array so that it can be used by other functions
+	"""
+
+	left_wheel = [float(t[1]) for t in wheel_speeds]
+	right_wheel = [float(t[2]) for t in wheel_speeds]
+	fig = plt.figure()
+	plt.plot(range(len(left_wheel)), left_wheel, 'b', label='Left Wheel')
+	plt.plot(range(len(right_wheel)), right_wheel, 'r', label='Right Wheel')
+	plt.ylim(-1, 1)
+	plt.xlabel('Times [ms]')
+	plt.ylabel('Velocity [m/s]')
+	plt.legend()
+	fig.savefig(individual_dir + '/wheel_speeds.png')
+	plt.close()
+
+
+def plot_spikes(individual_dir):
+	"""
+	Read the all_spikes csv file and plot the spikes emitted by each neuron
+	"""
+
+	file_path = individual_dir + '/all_spikes.csv'
+	with open(file_path, 'rb') as csv_file:
+		spike_reader = csv.reader(csv_file)
+		spikes = list(spike_reader)[1:]
+
+	for i in range(len(spikes)):
+		spikes[i][0] = int(float(spikes[i][0]))
+		spikes[i][1] = float(spikes[i][1])
+
+	spikes = [s for s in spikes if s[1] <= 40000]
+
+	neurons = column(spikes, 0)
+	times = column(spikes, 1)
+	fig = plt.figure()
+	plt.gca()
+	plt.ylim(2, 13)
+	plt.yticks(range(3, 14), range(1, 11))
+	plt.xlabel('Time [ms]')
+	plt.ylabel('Neuron ID')
+	plt.plot(times, neurons, 'bo')
+	fig.savefig(individual_dir + '/spikes.png')
+	plt.close()
+
+
+def column(matrix, i):
+	"""
+	helper function to return a column from a 2d array 
+	"""
+	return [row[i] for row in matrix]
