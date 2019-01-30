773d8746617b1347dbec4e052e456838c8ae1cf9
Mahmoud Akl <mahmoud.akl@gmail.com>
2019-01-30 12:47:52 +0100

    added husky robot model with modified camera parameters

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
-cp Models/brain_model/* $HBP/Models/brain_model
+cp Models/brain_model/* $HBP/Models/brain_model/
+cp Models/husky_model/* $HBP/Models/husky_model/
 cp -r Models/floreano_box $HBP/Models/
 cp -r Experiments/* $HBP/Experiments/
 
